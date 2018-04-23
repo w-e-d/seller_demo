4df1d2009bf775c92e5a4b6a270aa62841199983
wed <1271505916@qq.com>
2018-04-23 11:09:00 +0800

    goods页面

--- vue_demo/src/components/goods/goods.vue
+++ vue_demo/src/components/goods/goods.vue
@@ -1,13 +1,52 @@
 <template>
-	<div class="">
-      我是goods content
-    </div>
+	<div class="goods">
+		<div class="menu-wrapper">
+      
+	    </div>
+	    <div class="foods-wrapper">
+	    	
+	    </div>
+	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default {};
+	import axios from 'axios';
+
+	const ERR_OK = 0;
+
+	export default {
+		props:{
+			seller:{
+				type: Object
+			}
+		},
+		data() {
+			return {
+				goods: []
+			}
+		},
+		created() {
+			axios.get('/api/goods').then((res) => {
+				if(res.data.errno === ERR_OK){
+					this.goods = res.data.data;
+				}
+			})
+		}
+	};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-	
+	.goods
+		display: flex
+		position: absolute
+		top: 176px
+		bottom: 46px
+		width: 100%
+		overflow: hidden
+		.menu-wrapper
+			flex: 0 0 80px
+			width: 80px
+			background: #f3f5f7
+		.foods-wrapper
+			flex: 1
 </style>	
