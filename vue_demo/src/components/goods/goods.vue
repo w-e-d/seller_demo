918bbfab066b5cd80b7e63860234be5c7165c692
wed <1271505916@qq.com>
2018-07-02 19:52:41 +0800

    完善购物车样式

--- vue_demo/src/components/goods/goods.vue
+++ vue_demo/src/components/goods/goods.vue
@@ -35,7 +35,7 @@
 	    		</li>
 	    	</ul>
 	    </div>
-	    <shopcart></shopcart>
+	    <shopcart :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
 	</div>
 </template>
 
--- vue_demo/src/components/shopcart/shopcart.vue
+++ vue_demo/src/components/shopcart/shopcart.vue
@@ -3,22 +3,73 @@
 		<div class="content">
 			<div class="content-left">
 				<div class="logo-wrapper">
-					<div class="logo">
-						<span class="icon-shopping_cart"></span>
+					<div class="logo" :class="{'highlight': totalCount>0}">
+						<span class="icon-shopping_cart" :class="{'highlight': totalCount>0}"></span>
 					</div>
+					<div v-show="totalCount>0" class="num">{{totalCount}}</div>
 				</div>
-				<div class="price">￥48</div>
-				<div class="desc">另需配送费2元</div>
+				<div class="price" :class="{'highlight': totalPrice>0}">￥{{totalPrice}}</div>
+				<div class="desc">另需配送费{{deliveryPrice}}元</div>
 			</div>
 			<div class="content-right">
-				<div class="pay">￥12元</div>
+				<div class="pay">￥{{minPrice}}元起送</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script type="text/ecmascript-6">
-	export default{};
+	export default{
+		data() {
+			return {
+
+			}
+		},
+
+		props: {
+
+			selectFoods: {
+				type: Array,
+				default() {
+					return [
+						{
+							price: 10,
+							count: 1
+						}
+					];
+				}
+			},
+
+			deliveryPrice: {
+				type: Number,
+				default: 0
+			},
+
+			minPrice: {
+				type: Number,
+				default: 0
+			}
+		},
+
+		computed: {
+
+			totalPrice() {
+				let total = 0;
+				this.selectFoods.forEach((food) => {
+					total += food.price * food.count;
+				})
+				return total;
+			},
+
+			totalCount() {
+				let count = 0;
+				this.selectFoods.forEach((food) => {
+					count += food.count;
+				})
+				return count;
+			}
+		}
+	};
 </script>
 
 <style lang="stylus">
@@ -54,10 +105,28 @@
 						border-radius: 50%
 						text-align: center
 						background: #2b343c
+						&.highlight
+							background: rgb(0, 160, 220)
 						.icon-shopping_cart
 							line-height: 44px
 							font-size: 24px
 							color: #80858a
+							&.highlight
+								color: #fff
+					.num
+						position: absolute
+						top: 0
+						right: 0
+						width: 24px
+						height: 16px
+						line-height: 16px
+						text-align: center
+						border-radius: 16px
+						font-size: 10px
+						font-weight: 700
+						color: #fff
+						background: rgb(240, 20, 20)
+						box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
 				.price
 					display: inline-block
 					line-height: 24px
@@ -66,8 +135,10 @@
 					box-sizing: border-box
 					border-right: 1px solid rgba(255,255,255,.1)
 					font-size: 16px
-					color: #3688ff
+					color: rgba(255,255,255,.4)
 					font-weight: 700
+					&.highlight
+						color: #fff
 				.desc
 					display: inline-block
 					line-height: 24px
