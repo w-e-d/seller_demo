9cbba66a469121df587a6fc0640610dce05fd533
wed <1271505916@qq.com>
2018-07-12 18:38:52 +0800

    购物车功能优化

--- vue_demo/src/components/cartcontrol/cartcontrol.vue
+++ vue_demo/src/components/cartcontrol/cartcontrol.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="cartcontrol">
-		<div v-show="food.count>0" class="decrease icon-remove_circle_outline"></div>
-		<div v-show="food.count>0" class="count"></div>
+		<div v-show="food.count>0" @click="decreaseCart" class="decrease icon-remove_circle_outline"></div>
+		<div v-show="food.count>0" class="count">{{food.count}}</div>
 		<div @click="addCart" class="add icon-add_circle"></div>
 	</div>
 </template>
 
 <script>
+	import vue from 'vue'
+
 	export default{
 		data() {
 			return {
@@ -15,8 +17,21 @@
 		},
 
 		methods: {
-			addCart() {
-				console.log(111)
+			addCart(event) {
+				if(!event._constructed) return;
+				console.log(this.food.count)
+				if(!this.food.count){
+					vue.set(this.food, 'count', 1)
+				}else{
+					this.food.count++;
+				}
+			},
+
+			decreaseCart(event) {
+				if(!event._constructed) return;
+				if(this.food.count){
+					this.food.count--;
+				}
 			}
 		},
 
@@ -40,4 +55,10 @@
 			line-height: 24px
 			font-size: 24px
 			color: rgb(0, 160, 220)
+		.count
+			width: 12px
+			line-height: 24px
+			padding-top: 6px
+			font-size: 10px
+			color: rgb(147, 153, 159)
 </style>
